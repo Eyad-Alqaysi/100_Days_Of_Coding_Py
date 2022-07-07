5a36de54806d9dc5c9171c160709c6b2c7508246
Eyad Alqaysi <Eyad1423@icloud.com>
2022-07-08 00:19:12 +0300

    Update Day 8 Project

--- Lectures/Day 8/Codes/CaesarCipher.py
+++ Lectures/Day 8/Codes/CaesarCipher.py
@@ -1,17 +1,21 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def caesar(text, shift, direction):
     s = ""
     if direction == "decode":
         shift *= -1
     for i in text:
-        position = alphabet.index(i)
-        new_position = position + shift
-        s += alphabet[new_position]
+        if i in alphabet:
+            position = alphabet.index(i)
+            new_position = position + shift
+            s += alphabet[new_position]
+        else:
+            s += i
     print(f"Here's the {direction}d result: {s}")
-
-caesar(text,shift,direction)
+stop =''
+while stop != 'yes':
+    direction = input("Type 'encode' to encrypt, type 'decode'  to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text,shift,direction)
+    stop = input("Type 'yes' if you want to stop the program. Otherwise type 'no'.\n")
